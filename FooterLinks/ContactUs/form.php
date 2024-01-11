0e81bb666697d9367c57b9cbca8cc61fb5fc9916
Sportsoracle05 <119137579+Sportsoracle05@users.noreply.github.com>
2024-01-11 21:41:10 +0100

    Add files via upload

---
+++ FooterLinks/ContactUs/form.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Open a file to write form data
+    $file = 'contact_data.txt';
+    $current = file_get_contents($file);
+
+    // Append form data to the file
+    $current .= "Name: " . $name . "\nEmail: " . $email . "\n\nSubject: " . $subject . "\n\n\nMessage: " . $message . "\n\n\n\n";
+    file_put_contents($file, $current);
+}
+?>
