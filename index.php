dd6be7a6524d87302995f3f486dcdc3f96c6de82
Sportsoracle05 <119137579+Sportsoracle05@users.noreply.github.com>
2024-01-12 08:33:11 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require_once "utils/common.php";
+
+$uri = getRootURI();
+
+switch ($_SERVER['REQUEST_URI']) {
+
+    case $uri . '':
+    case $uri . '/':
+        require __DIR__ . '/views/index.php';
+        break;
+
+    case $uri . '/projects/list':
+        require __DIR__ . '/controllers/project_list.php';
+        break;
+
+    case $uri . '/tasks/list':
+        require __DIR__ . '/controllers/task_list.php';
+        break;
+
+    case $uri . '/projects/add':
+        require __DIR__ . '/controllers/project.php';
+        break;
+
+    case $uri . '/tasks/add':
+        require __DIR__ . '/controllers/task.php';
+        break;
+
+    case $uri . '/reports':
+        require __DIR__ . '/controllers/reports.php';
+        break;
+
+        // case preg_match($uri . '/\/projects\/\?id=\d/', $_SERVER['REQUEST_URI']):
+        //     require __DIR__ . '/controllers/project.php';
+        //     break;
+
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
